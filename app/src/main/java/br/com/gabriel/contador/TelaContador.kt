d60d67ecd1d5086c74fc67f8785c1a0bbef7a8cf
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2024-10-01 21:58:36 -0300

    added logic

--- app/src/main/java/br/com/gabriel/contador/TelaContador.kt
+++ app/src/main/java/br/com/gabriel/contador/TelaContador.kt
@@ -1,6 +1,8 @@
 package br.com.gabriel.contador
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,18 @@ class TelaContador : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val textView = findViewById<TextView>(R.id.textView);
+        val btnAumentar = findViewById<Button>(R.id.btnAumentar);
+        val btnDiminuir = findViewById<Button>(R.id.btnDiminuir);
+
+        var contador = 0;
+
+        btnAumentar.setOnClickListener(){
+            textView.setText(++contador);
+        }
+        btnDiminuir.setOnClickListener(){
+            textView.setText(--contador);
+        }
+
     }
 }
